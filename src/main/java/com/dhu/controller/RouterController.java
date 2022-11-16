d811fdb8ba65b115800897f281027cb81511da28
DhuShen <massd2002@163.com>
2022-11-16 18:06:24 +0800

    Init commit

---
+++ src/main/java/com/dhu/controller/RouterController.java
@@ -0,0 +1,12 @@
+package com.dhu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RouterController {
+    @RequestMapping("/LoginPage")
+    public String login() {
+        return "login";
+    }
+}
